1fd435cce4d4a5dfbc2f524d66c131ed035bde83
NJ9899 <86415912+NJ9899@users.noreply.github.com>
2022-09-05 14:23:52 +0530

    Add files via upload

---
+++ JAVA/Day3/String_substring.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class String_substring {
+	
+	public static void main(String[] args) {
+		
+		String str1="program";
+		
+		//from 1st to the 7th character
+		System.out.println(str1.substring(0,7));	//program
+		
+		//from 1st to the 5th character
+		System.out.println(str1.substring(0,5));	//progr
+		
+		//from 4st to the 5th character
+		System.out.println(str1.substring(3,5));	//gr
+	}
+
+}

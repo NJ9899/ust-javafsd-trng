7ef7033281f5519d266dbcb9e599e76008eb58c8
NJ9899 <86415912+NJ9899@users.noreply.github.com>
2022-10-26 10:24:59 +0530

    Update sba6.java

--- SBA/WEEK 6/sba6.java
+++ SBA/WEEK 6/sba6.java
@@ -9,12 +9,13 @@ public class sba6 {
 	static Logger log = Logger.getLogger(sba6.class);
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		log.info("Print info");
-		log.debug("Debug message");
-		log.trace("trace message");
 		log.warn("Warning message");
 		log.error("Error message");
 		log.fatal("fatal message");
+		log.info("Print info");
+		log.debug("Debug message");
+		log.trace("trace message");
+		
 
 	}
 

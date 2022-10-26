23ecac953df89116aacda6f07ee4428c921bde4b
NJ9899 <86415912+NJ9899@users.noreply.github.com>
2022-10-26 09:52:36 +0530

    Add files via upload

---
+++ SBA/WEEK 6/sba6.java
@@ -0,0 +1,21 @@
+package sba;
+
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.BasicConfigurator;
+
+public class sba6 {
+
+	static Logger log = Logger.getLogger(sba6.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		log.info("Print info");
+		log.debug("Debug message");
+		log.trace("trace message");
+		log.warn("Warning message");
+		log.error("Error message");
+		log.fatal("fatal message");
+
+	}
+
+}
